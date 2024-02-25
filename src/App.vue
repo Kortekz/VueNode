49b1a7bc45666ce8291a576aa6773f3c767e5040
Kortekz <87400262+Kortekz@users.noreply.github.com>
2024-02-25 16:36:22 +0200

    first

---
+++ src/App.vue
@@ -0,0 +1,43 @@
+<template>
+  <NavBar/>
+  <router-view/>
+</template>
+
+
+
+<script>
+import NavBar from './components/NavBar.vue';
+
+export default {
+  components: {
+    NavBar,
+  }
+}
+
+</script>
+<style>
+#app {
+  font-family: 'Poppins', sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  
+}
+body{
+  background-color: #E3FFE0 !important;
+  overflow-x: hidden;
+}
+nav {
+  padding: 30px;
+}
+
+nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+nav a.router-link-exact-active {
+  color: #42b983;
+}
+</style>
---
+++ src/components/NavBar.vue
@@ -0,0 +1,93 @@
+<template>
+
+    <nav class="navbar navbar-expand-lg navbar-dark bg-transparent">
+      <a class="navbar-brand" href="../views/HomeView.vue">
+        <span class="logo">Kittleys</span>
+      </a>
+  
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+  
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav mx-auto">
+          <li class="nav-item active">
+            <a class="nav-link" href="../views/HomeView.vue">Home </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="../views/AboutView.vue">About</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="../views/ProductsView.vue">Products</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="../views/AdminView.vue">Admin</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="../views/ContactView.vue">Contact</a>
+          </li>
+        </ul>
+  
+        <!-- CONTACT US button  -->
+        <button class="btn btn-primary ml-auto">Contact Us</button>
+      </div>
+    </nav>
+  </template>
+  
+  <script>
+  export default {
+   
+  }
+  </script>
+  
+  <style scoped>
+
+
+  /* Custom styling for the navbar */
+  .navbar {
+    background-color: transparent !important;
+    padding-top: 20px;
+  }
+  
+  .logo {
+    margin-left: 50px;
+    font-size: 30px !important;
+  }
+  
+  .navbar-brand .logo {
+    font-size: 1.5rem;
+    font-weight: bold;
+    color: black !important;
+  }
+  
+  .navbar-nav {
+    
+    text-align: center; /* Center the navigation links */
+  }
+  
+  .navbar-nav .nav-item {
+    margin-right: 15px; /* Adjust the spacing between navigation links */
+  }
+  
+  .navbar-nav .nav-item .nav-link {
+    color: black !important;
+  }
+  
+  /* Style for the CONTACT US button */
+  .btn-primary {
+    background-color: #87EF97;
+    padding: 13px;
+    color: rgb(0, 0, 0);
+    border: none;
+    margin-right: 80px;
+    border-radius: 15px;
+    font-weight: 500;
+  }
+  .btn-primary:hover{
+    background-color: #6bad75;
+    color: rgb(0, 0, 0);
+  }
+  
+  
+  </style>
+  
